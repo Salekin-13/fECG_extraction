ee0165e841c5cb0ffc82b6a66714103dcff51e89
Sumaiya Salekin <sumaiya.salekin@bracu.ac.bd>
2026-04-06 12:54:00 +0600

    RLS updates

--- filters.py
+++ filters.py
@@ -1,53 +1,51 @@
 import numpy as np
 
 class RLS(object):
-    """For initialization, if no priori knowledge is known: 
-        P0=cI, where c is a large number
-        X0 = random value"""
+    """For initialization, if no priori knowledge is known:
+        P(0) = delta*I 
+        delta is initialized as a small +ve constant in dsp slide (gamma = delta_inv 1e9)
+        weight initialization, w(0) = 0 (dsp slide)"""
     
-    def __init__(self, P0, x0, R):
-        self.P0 = P0
-        self.x0 = x0
-        self.R = R #measurement noise covariance matrix
+    def __init__(self, delta, w0):
+        self.delta = delta
+        self.w0 = w0
+        #self.R = R #measurement noise covariance matrix
+        #disregard R for now
 
         self.currentTimestep_n = 0
 
-        self.x_estimates = []
-        self.x_estimates.append(x0) #this is what is being estimated by the filter
+        self.w_estimates = []
+        self.w_estimates.append(w0) #this is what is being estimated by the filter: the weights
 
-        #gain matrices
-        self.gain_mats = [] #redundant
 
         #estimation error x-x'_k covariance matrix
         self.P_estimates = []
+        P0 = np.matmul(delta, np.eye(np.size(w0)))
         self.P_estimates.append(P0)
 
-        #estimated error matrices
-        self.errors = [] #redundant
 
 
 
-    def weight_update(self, measurement_y, C):
+    def weight_update(self, measurement_d, u, lamda):
         #from eq 47: L(n) = R(n) + C(n)P(n-1)CT(n)
-        #C is probably the weight matrix
+        #C is probably the input matrix
 
-        Lmatrix = self.R + np.matmul(C, np.matmul(self.P_estimates[self.currentTimestep_n], C.T))
+        Lmatrix = lamda + np.matmul(u.T, np.matmul(self.P_estimates[self.currentTimestep_n], u))
         Lmatrix_inv = np.linalg.inv(Lmatrix)
 
         #gain matrix calculation from eqn 47: K(n) = P(n-1)CT(n) L-1(n)
-        gain_K = np.matmul(self.P_estimates[self.currentTimestep_n], np.matmul(C.T, Lmatrix_inv)) #include lambda later
-        self.gain_mats.append(gain_K)
+        gain_K = np.matmul(self.P_estimates[self.currentTimestep_n], np.matmul(u, Lmatrix_inv)) #include lambda later
 
-        #calculate the error: e(n) = y(n) - x^(n)
-        error = measurement_y - np.matmul(C, self.x_estimates[self.currentTimestep_n])
-        self.errors.append(error)
+        #calculate the estimation error: alpha(n) = d(n) - wT(n-1)u(n)
+        error = measurement_d - np.matmul(u.T, self.w_estimates[self.currentTimestep_n]) 
 
-        x_update = self.x_estimates[self.currentTimestep_n] + np.matmul(gain_K, error)
-        self.x_estimates.append(x_update)
+        w_update = self.w_estimates[self.currentTimestep_n] + np.matmul(gain_K, error) #this looks like weight update
+        #w(n) = w(n-1) + k(n)a(n)
+        self.w_estimates.append(w_update)
 
         #updating error covariance matrix from eq 50: P(n) = [I - K(n)C(n)]P(n-1)
-        P_update = np.matmul(np.eye(np.size(self.x0), np.size(self.x0)) - np.matmul(gain_K, C), self.P_estimates[self.currentTimestep_n])
-        self.P_estimates.append(P_update)
+        P_update = self.P_estimates[self.currentTimestep_n] - np.matmul(gain_K, np.matmul(u.T, self.P_estimates[self.currentTimestep_n]))
+        self.P_estimates.append(np.matmul(lamda, P_update))
 
         self.currentTimestep_n = self.currentTimestep_n + 1
 
